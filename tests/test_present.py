1f5bbdfca7a3afbb703ff627a1528b5c8b462853
kathleenfryer <kathleenbell96@gmail.com>
2025-12-02 12:38:02 +0000

    present and test_present completed

--- tests/test_present.py
+++ tests/test_present.py
@@ -1,3 +1,34 @@
 from lib.present import * 
 import pytest
 
+def test_wrap_and_unwrap():
+    """
+    test function works with an item
+    """
+    present = Present()
+    present.wrap("item")
+    result = present.unwrap()
+    assert result == "item" 
+
+
+def test_contents_already_wrapped():
+    """
+    test exception contents wrapped
+    """
+    present = Present()
+    present.contents = "item"
+    with pytest.raises(Exception) as e:
+        present.wrap(present.contents)
+    error_message = str(e.value)
+    assert error_message == "A contents has already been wrapped."
+
+def test_unwrap_empty():
+    """
+    test exception no contents wrapped
+    """
+    present = Present()
+    with pytest.raises(Exception) as e:
+        present.unwrap()
+    error_message = str(e.value)
+    assert error_message == "No contents have been wrapped."
+    
