1fd2c3f151b8357ee8b412b1df2cc55a226062e3
kathleenfryer <kathleenbell96@gmail.com>
2025-12-03 12:27:03 +0000

    make_snippet edge cases added

--- tests/test_make_snippet.py
+++ tests/test_make_snippet.py
@@ -9,3 +9,13 @@ def test_shorten_phrase_over_5_words():
     expected = "this phrase is too long..."
     actual = make_snippet("this phrase is too long in all honesty")
     assert expected == actual 
+
+def test_less_than_5_words():
+    expected = "Hello"
+    actual = make_snippet("Hello")
+    assert expected == actual
+
+def test_empty_string():
+    expected = ""
+    actual = make_snippet("")
+    assert expected == actual 
